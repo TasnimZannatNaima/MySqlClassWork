473a024257694773244ab374de16833d244f8efb
Tasnim Zannat Naima <naimatasnimzannat@gmail.com>
2025-06-23 23:37:19 +0600

    Update ConsoleInput.java

--- src/ConsoleInput.java
+++ src/ConsoleInput.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class ConsoleInput {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static int getInt(String prompt) {
+        System.out.print(prompt);
+        while (!scanner.hasNextInt()) {
+            System.out.print("Invalid input. Please enter a number: ");
+            scanner.next();
+        }
+        return scanner.nextInt();
     }
+
+    public static String getString(String prompt) {
+        System.out.print(prompt);
+        return scanner.next();
+    }
+}
+
